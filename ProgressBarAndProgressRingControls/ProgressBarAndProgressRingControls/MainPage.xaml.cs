af2618c5a176bd756c892f9783229279e4e0c5f2
scriptnull <vishnubharathi@live.com>
2014-06-03 22:13:39 +0530

    Progress Bar and Progress Ring added

---
+++ ProgressBarAndProgressRingControls/ProgressBarAndProgressRingControls/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ProgressBarAndProgressRingControls
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            //start the progress bar animation
+            pbMyProgress.IsIndeterminate = true;
+
+            //start the progress ring animation 
+            prMyProgress.IsActive = true;
+
+
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            //stop the progress bar animation
+            pbMyProgress.IsIndeterminate = false;
+
+            //stop the progress ring animation 
+            prMyProgress.IsActive = false;
+
+        }
+    }
+}
